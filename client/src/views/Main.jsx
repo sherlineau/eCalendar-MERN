d2dc98942f3fc0d205f4822c25a688428063ba17
Sherline Au <au.sherline@gmail.com>
2023-01-01 15:48:22 -0600

    moved day state to main parent

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -2,10 +2,11 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Calendar from '../components/Calendar'
-import DaySquare from '../components/DaySquare'
 
 const Main = () => {
   const [appointments, setAppointments] = useState([])
+  const [day, setDay] = useState(new Date());
+
   const heading = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
   const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -16,9 +17,14 @@ const Main = () => {
       .catch(err => console.log(err))
   })
 
+  const handleClick = (day) =>
+  {
+    setDay(day)
+  }
+
   return (
-    <div>
-      <Calendar appointments={appointments} heading={heading} weekDays={weekDays} />
+    <div className="section">
+      <Calendar appointments={appointments} heading={heading} weekDays={weekDays} onClickProp={handleClick}/>
     </div>
   )
 }
