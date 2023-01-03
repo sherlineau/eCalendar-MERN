96f1852ed661bfab6d2e837c97904bd016219297
Sherline Au <au.sherline@gmail.com>
2023-01-03 17:49:25 -0600

    moved and connected form to modal in Appoint-Panel

--- client/src/components/AppointmentsPanel.jsx
+++ client/src/components/AppointmentsPanel.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import AppointmentsHeader from "./AppointmentsHeader";
+import FormModal from "./FormModal";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const AppointmentsPanel = (props) => {
   const { heading, weekDays, day, appointments } = props;
+  const [showModal, setShowModal] = useState(false);
   const month = day.getMonth();
 
   // set background image based on month's season
@@ -51,50 +54,65 @@ const AppointmentsPanel = (props) => {
 
   let filteredAppointments = [];
   for (let i = 0; i < appointments.length; i++) {
-    let temp = new Date(appointments[i].start)
+    let temp = new Date(appointments[i].start);
     if (temp.toDateString() === day.toDateString()) {
       filteredAppointments.push(appointments[i]);
     }
   }
-  
+
+  const closeModal = (modal) => {
+    setShowModal(false);
+  };
+
   return (
     <div className={`section-right ${season}`}>
-        <AppointmentsHeader heading={heading} weekDays={weekDays} day={day} />
+      <AppointmentsHeader heading={heading} weekDays={weekDays} day={day} />
       <div className="events">
-        <div className="line"></div>
-        <div className="event-list">
-          <table>
-            {filteredAppointments.length === 0 ? (
-              <thead>
-                <tr>
-                  <th>No Appointments</th>
-                </tr>
-              </thead> 
-            ): (
-              filteredAppointments.map((i,index) => {
-                let temp = new Date(i.start)
-                // format into readable time
-                let time = ""
-                if ( temp.getHours() > 12) {
-                  time = `${temp.getHours()-12}:`
-                } else {
-                  time += `${temp.getHours()}:`
-                }
-                if (temp.getMinutes() < 10) {
-                  time += `0${temp.getMinutes()}`
-                } else {
-                  time += temp.getMinutes()
-                }
-                temp.getHours() > 12 ? time+=" PM": time+=" AM"
-                return (
-                  <tr key={index}>
-                    <td>{`${time}`}</td>
-                    <td>{i.title}</td>
+        <div className="top">
+          <div className="line"></div>
+          <div className="event-list">
+            <table>
+              {filteredAppointments.length === 0 ? (
+                <thead>
+                  <tr>
+                    <th>No Appointments</th>
                   </tr>
-                )
-              })
-            )}
-          </table>
+                </thead>
+              ) : (
+                filteredAppointments.map((i, index) => {
+                  let temp = new Date(i.start);
+                  // format into readable time
+                  let time = "";
+                  if (temp.getHours() > 12) {
+                    time = `${temp.getHours() - 12}:`;
+                  } else {
+                    time += `${temp.getHours()}:`;
+                  }
+                  if (temp.getMinutes() < 10) {
+                    time += `0${temp.getMinutes()}`;
+                  } else {
+                    time += temp.getMinutes();
+                  }
+                  temp.getHours() > 12 ? (time += " PM") : (time += " AM");
+                  return (
+                    <tr key={index}>
+                      <td>{`${time}`}</td>
+                      <td>{i.title}</td>
+                    </tr>
+                  );
+                })
+              )}
+            </table>
+          </div>
+        </div>
+        <div className="bottom">
+          <AiOutlinePlus
+            className="plus-icon"
+            onClick={(e) => setShowModal(!showModal)}
+          />
+        </div>
+        <div className={`modal ${showModal ? "show-modal" : ""}`}>
+          <FormModal onSubmitProp={closeModal} onClickProp={closeModal}/>
         </div>
       </div>
     </div>
--- client/src/components/FormModal.jsx
+++ client/src/components/FormModal.jsx
@@ -1,58 +1,157 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
-const FormModal = () => {
-  const [title, setTitle] = useState("")
-  const [startDate, setStartDate] = useState("")
-  const [endDate, setEndDate] = useState("")
-  const [startTime, setStartTime] = useState("")
-  const [endTime, setEndTime] = useState("")
-  const [people, setPeople] = useState("")
-  const [location, setLocation] = useState("")
-  const [description, setDescription] = useState("")
+const FormModal = (props) => {
+  const { onSubmitProp, onClickProp } = props;
+  const [title, setTitle] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [people, setPeople] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    let start = new Date(`${startDate}T${startTime}`)
-    let end = new Date(`${endDate}T${endTime}`)
-    console.log({title,start,end,people,location,description});
+    e.preventDefault();
+    let start = new Date(`${startDate}T${startTime}`);
+    let end = new Date(`${endDate}T${endTime}`);
     axios
-      .post(`http://localhost:8000/api/appointments`, 
-        { title, start, end, people, location, description })
-      .then( res => console.log(res))
-      .catch( err => console.log(err))
-  }
-  
+      .post(`http://localhost:8000/api/appointments`, {
+        title,
+        start,
+        end,
+        people,
+        location,
+        description,
+      })
+      .then((res) => onSubmitProp(res.json))
+      .catch((err) => {
+        // temp variable to store all our messages
+        const errorMessages = {};
+        const errorResponse = err.response.data.errors;
+        for (const key in errorResponse) {
+          errorMessages[key] = errorResponse[key].message;
+        }
+        setErrors(errorMessages);
+      });
+  };
+
+  const handleClick = (e) => {
+    onClickProp(false);
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Title</label>
-      <input type="text" value={title} onChange={ e => setTitle(e.target.value) } />
-      
-      <label>Start Date</label>
-      <input type="date"  value={startDate} onChange={ e => setStartDate(e.target.value) }/>
-    
-      <label>End Date</label>
-      <input type="date"  value={endDate} onChange={ e => setEndDate(e.target.value) }/>
-    
-      <label>Start Time</label>
-      <input type="time"  value={startTime} onChange={ e => setStartTime(e.target.value) }/>
-    
-      <label>End Time</label>
-      <input type="time"  value={endTime} onChange={ e => setEndTime(e.target.value) }/>
-    
-      <label>People</label>
-      <input type="text" value={people} onChange={ e => setPeople(e.target.value) } />
-      
-      <label>Location</label>
-      <input type="text" value={location} onChange={ e => setLocation(e.target.value) } />
-      
-      <label>Description</label>
-      <input type="text" value={description} onChange={ e => setDescription(e.target.value) } />
-      
-      <button>Submit</button>
-    </form>
-  )
-}
+    <div className="form-container">
+      <h1>Create Appointment/Event</h1>
+      <form onSubmit={handleSubmit}>
+        <label className="form-label mt-3">
+          Title
+          {errors["title"] ? (
+            <span style={{ color: "red" }}>{errors["title"]}</span>
+          ) : (
+            ""
+          )}
+        </label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="form-control"
+        />
 
-export default FormModal
+        <label className="form-label mt-3">
+          Start Date
+          {errors["start"] ? (
+            <span style={{ color: "red" }}>{errors["start"]}</span>
+          ) : (
+            ""
+          )}
+        </label>
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">
+          End Date
+          {errors["end"] ? (
+            <span style={{ color: "red" }}>{errors["end"]}</span>
+          ) : (
+            ""
+          )}
+        </label>
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">
+          Start Time
+          {errors["start"] ? (
+            <span style={{ color: "red" }}>{errors["start"]}</span>
+          ) : (
+            ""
+          )}
+        </label>
+        <input
+          type="time"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">
+          End Time
+          {errors["end"] ? (
+            <span style={{ color: "red" }}>{errors["end"]}</span>
+          ) : (
+            ""
+          )}
+        </label>
+        <input
+          type="time"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">People</label>
+        <input
+          type="text"
+          value={people}
+          onChange={(e) => setPeople(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">Location</label>
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className="form-control"
+        />
+
+        <label className="form-label mt-3">Description</label>
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="form-control"
+        />
+
+        <div className="mt-3 btn-group">
+          <button onClick={(e) => handleClick()} className="btn btn-outline-danger">Cancel</button>
+          <button type="submit" className="btn btn-outline-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormModal;
