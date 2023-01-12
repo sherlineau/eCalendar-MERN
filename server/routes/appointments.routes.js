95cec710e2b4c8a55b0dc93c87353938cba50b4b
Sherline Au <au.sherline@gmail.com>
2023-01-12 14:34:22 -0600

    added auth to appointment routes

--- server/routes/appointments.routes.js
+++ server/routes/appointments.routes.js
@@ -1,9 +1,10 @@
+const { auth } = require("../config/jwt.config");
 const AppointmentController = require("../controllers/appointments.controller");
 
 module.exports = (app) => {
-  app.post(`/api/appointments/new`, AppointmentController.createAppointment);
+  app.post(`/api/appointments/new`, auth, AppointmentController.createAppointment);
   app.get(`/api/appointments`, AppointmentController.getAllAppointments);
   app.get(`/api/appointments/:_id`, AppointmentController.getOneAppointment);
-  app.put(`/api/appointments/:_id`, AppointmentController.updateAppointment);
-  app.delete(`/api/appointments/:_id`, AppointmentController.deleteAppointment);
+  app.put(`/api/appointments/:_id`, auth, AppointmentController.updateAppointment);
+  app.delete(`/api/appointments/:_id`, auth, AppointmentController.deleteAppointment);
 };
