497f342bbc666e90543b68b0709acd9ca9c2d12e
Jay T <jaytula@gmail.com>
2023-01-16 15:56:06 -0800

    Add proxy to client

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
 
 const app = express();
 
@@ -18,6 +20,21 @@ app.set("port", process.env.PORT || 3000);
 // routes
 require("./routes/user.routes")(app);
 require("./routes/appointments.routes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+} else {
+  app.use(
+    "/",
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    })
+  );
+}
 
 // Listen
 app.listen(app.get("port"), () => {
