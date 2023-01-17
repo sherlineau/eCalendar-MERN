07bd13c01a92043a0039ba3eb4834332297ff895
Jay T <jaytula@gmail.com>
2023-01-16 17:27:39 -0800

    Use path.resolve instead

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ require("./routes/appointments.routes")(app);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, '..', 'client', "build", "index.html"));
   });
 } else {
   app.use(
